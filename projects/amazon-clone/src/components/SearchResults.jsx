667c37404b0c839e1670cdbe987f55bcfc3d6700
SatyadeepGohil <gohilsatyadeep7@gmail.com>
2024-10-24 17:03:27 +0530

    making changes in search results

--- projects/amazon-clone/src/components/SearchResults.jsx
+++ projects/amazon-clone/src/components/SearchResults.jsx
@@ -1,7 +1,5 @@
 import { BrowserRouter as Router, useLocation } from "react-router-dom";
 import { useEffect } from "react";
-import axios from "axios";
-
 import Header from './header';
 import NavBar from './navbar';
 import Footer from './footer';
@@ -9,74 +7,64 @@ import '../SCSS/main.css';
 import { useState } from "react";
 
 function SearchResults() {
-    const location = new useLocation();
-    const searchParams =  new URLSearchParams(location.search)
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
     const query = searchParams.get('q');
     const [results, setResults] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        const fecthData = async () => {
-            const options = {
-                method: 'GET',
-                url: 'https://real-time-amazon-data.p.rapidapi.com/search',
-                params: {
-                    query: query,
-                    page: '1',
-                    country: 'US',
-                    sort_by: 'RELEVANCE',
-                    product_condition: 'ALL',
-                    is_prime: 'false',
-                    deals_and_discounts: 'NONE'
-                },
-                headers: {
-                    'x-rapidapi-key': 'e3592d4877mshed0912fb419364bp1a6f16jsnc8d3aa1a9635',
-                    'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com'
-                }
-            };
+      const fetchData = async () => {
+        try {
+          setLoading(true)
+          const response = await fetch('https://fakestoreapi.com/products');
+          if (!response.ok) {
+            throw new Error('Failed to fetch');
+          }
 
-            try {
-                const response = await axios.request(options);
-                setResults(response.data);
-                setLoading(false);
-            } catch (error) {
-                setError(error.message);
-                setLoading(false);
-            };
-        };
+          const data = await response.json();
 
-        if (query) {
-            fecthData();
+          const filteredResults = data.filter(item => item.title.toLowerCase().includes(query.toLowerCase()));
+          setResults(filteredResults)
+        } catch (err) {
+          setError(err.message);
+        } finally {
+          setLoading(false);
         }
+      };
+
+      fetchData();
     }, [query]);
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error}</p>;
 
-    if (loading) return <p>Loading...</p>
-    if (error) return <p>Error : {error}</p>
+    return (
+        <>
+            <Header />
+            <NavBar />
+            <div>
+                <h1>{`${results.length} results for ${query}`}</h1>
+                <div id="search-result-container">
+                    {results && results.length > 0 ? (
+                        results.map((item, index) => (
+                            <div key={index} className="search-result-card">
+                                <img src={item.image} alt={item.title}/>
+                                <h2>{item.title}</h2>
+                                <p>₹{item.price}</p>
+                                <p>{item.category}</p>
 
-  return (
-    <>
-        <Header />
-        <NavBar />
-        <div>
-        <h1>Search Results for: {query}</h1>
-        <div>
-          {results && results.length > 0 ? (
-            results.map((item, index) => (
-              <div key={index}>
-                <h2>{item.title}</h2>
-                <p>{item.price}</p>
-                {/* Render other product details as needed */}
-              </div>
-            ))
-          ) : (
-            <p>No results found</p>
-          )}
-        </div>
-      </div>
-        <Footer />
-    </>
-  );
+                                <button>Add to cart</button>
+                            </div>
+                        ))
+                    ) : (
+                        <p>No results found</p>
+                    )}
+                </div>
+            </div>
+            <Footer />
+        </>
+    );
 }
 
 export default SearchResults;
